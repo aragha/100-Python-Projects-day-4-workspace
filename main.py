04b2f414ce6832a60326d1322b7121dd5728cd4c
resplit <67502036+aragha@users.noreply.github.com>
2022-11-17 22:11:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+import mymodule
+# generate random whole number
+random_integer = random.randint(1, 10)
+print(random_integer)
+# print(mymodule.pi)
+
+# generate random float number
+random_float = random.random()
+print(random_float)
+# generate random floats between 0 and 5
+random_float = random.random() * 5
+print(random_float)
